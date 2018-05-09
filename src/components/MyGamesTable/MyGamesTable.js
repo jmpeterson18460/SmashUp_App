14cdf8e392bde8c0044e6501e255dd308bcc762e
Justin <jmpeterson9891@gmail.com>
2018-05-09 15:32:17 -0500

    Styled the tables on the MyGameLogPage.js andadd code comments to MyGamesTable.js

--- src/components/MyGamesTable/MyGamesTable.js
+++ src/components/MyGamesTable/MyGamesTable.js
@@ -20,26 +20,46 @@ const mapStateToProps = state => ({
       
     render(){
 
-        //gameArray is an array whose elements are arrays; these array elements 
-        //contain games logged by the user
+        //gameArray is an array whose elements are arrays; the elements of those arrays 
+        //contain the game information of each player from a given game
         let gameArray = this.props.state.faction.gameId.map((gameId) => {
+            //this.props.state.faction.gameId.map maps over an array of game
+            //ids from the database
+
+            //gameIdTest is the conditional that compares the game id from the array
+            //of game ids to the game id of a game from the database
             const gameIdTest = (item => item.game_id === gameId.id)
+
+            //newArray is an array of game information of each player from
+            //the same game
             let newArray = this.props.state.faction.myGames.filter(gameIdTest)
             return newArray
         })
-        console.log('gameArray: ', gameArray);
 
         let gameTable = [];
 
+        //the result of this for loop will be the array gameTable whose elements are arrays;
+        //inside those arrays are table rows
         for(let games of gameArray){
+
+            //myGame is an array of table rows
             let myGame = games.map((game) => {
+
+            //games is an array from gameArray whose elements are the game information of each
+            //player that played that game
                 return (<tr key={game.id}><td>{game.player_name}</td><td>{game.faction1} / {game.faction2}
                         </td><td>{game.points}</td><td>{game.rank}</td></tr>)
             })
+
+            //puts myGame into gameTable
             gameTable.push(myGame)
         }
 
+        //finalTable is an array of tables; these tables contain the player information of
+        //each player as rows from a given game
         let finalTable = gameTable.map((table) => {
+
+            //gameTable is an array whose elements are arrays; inside those arrays are table rows
             return(<table><thead><tr><th>Players</th><th>Factions</th><th>Points</th><th>Rank</th>
                     </tr></thead><tbody>{table}</tbody></table>)
         })
